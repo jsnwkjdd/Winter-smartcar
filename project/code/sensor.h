9ea6b3b54ecdbc6fb8e5484a8b91b2e47e862e95
VeraWatermelon <19730032635@163.com>
2026-01-25 17:03:08 +0800

    红外传感器初始化及数据读取

---
+++ project/code/sensor.h
@@ -0,0 +1,12 @@
+#ifndef __SENSOR_H
+#define __SENSOR_H
+#include <stdint.h>
+void sensor_init(void);
+uint8_t sensor(void);
+uint8_t IRSensorL1_Get(void);
+uint8_t IRSensorL2_Get(void);
+uint8_t IRSensorR2_Get(void);
+uint8_t IRSensorR1_Get(void);
+
+
+#endif
