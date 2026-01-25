b00be083df2372c3be5e78b6a8d2d4705ce8cf02
1936705527 <3093125288@qq.com>
2026-01-25 11:17:17 +0800

    菜单框架与LED控制

---
+++ code/LED.c
@@ -0,0 +1,19 @@
+#include "zf_driver_gpio.h"
+
+//LED初始化函数
+void LED_init(void)
+{
+	 gpio_init(H2, GPO, GPIO_LOW, GPO_PUSH_PULL);                             // 初始化 LED1 输出 默认高电平 推挽输出模式
+}
+
+//LED控制灯亮函数
+void LED_up(void)
+{
+	gpio_set_level(H2, GPIO_HIGH);
+}
+
+//LED控制灯灭函数
+void LED_down(void)
+{
+	gpio_set_level(H2, GPIO_LOW);
+}
---
+++ code/menu.h
@@ -0,0 +1,11 @@
+#ifndef __menu_H_
+#define __menu_H_
+
+#include "zf_device_tft180.h"
+#include "key.h"
+
+extern float kp;
+extern float ki;
+extern float kd;
+
+#endif
