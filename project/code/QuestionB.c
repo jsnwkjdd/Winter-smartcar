c1b819d786ef5a45b8fb5009528f1c6842803518
VeraWatermelon <19730032635@163.com>
2026-01-29 15:35:15 +0800

    添加蜂鸣器和led

--- project/code/QuestionB.c
+++ project/code/QuestionB.c
@@ -1,6 +1,6 @@
 
 #include "sensor.h"
-uint8_t B_State,count_B;
+uint8_t B_State,count_B,flag_beep_led=0;
 uint8_t qestionB(void)
 {
 	if(sensor()==0)//如果初始位置在白线，进入题目模式，并计次4
@@ -22,14 +22,27 @@ uint8_t qestionB(void)
 				count_B--;
 				control();//循迹
 				if(sensor()==0)//检测到全白线时，切换至题目模式
-				{B_State=2;}
+				{
+				
+					B_State=2;
+					//蜂鸣器+led提示
+				}
 			}
 			else if(B_State==2)
 			{
 				count_B--;
 				//题目
+				
+				
+				
+				
+				
 				if(sensor()!=0)//检测到黑线时，切换至循迹模式
-				{B_State=1;}
+				{
+				
+					B_State=1;
+					//蜂鸣器+led提示
+				}
 			}
 		
 		}
