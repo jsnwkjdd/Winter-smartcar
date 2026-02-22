42373a3db7ff19c604c2d8f4bf1ff1bb99631c9a
VeraWatermelon <19730032635@163.com>
2026-02-22 20:18:50 +0800

    1

--- project/code/mpu6050.c
+++ project/code/mpu6050.c
@@ -4,7 +4,7 @@
 float gyro_x,gyro_y,gyro_z,acc_x,acc_y,acc_z,gyro_y1;//数据处理中间量
 float AX,AY,AZ,GX,GY,GZ,AngleX,AngleY,AngleZ;
 int16_t ax,az,gy;//互补滤波中间量
-float AlphaX = 0.001,AlphaPitch = 0.018;//互补滤波参数
+float AlphaX = 0.001,AlphaPitch = 0.025;//互补滤波参数
 float t=0.01;//t角速度积分，和定时中断同步
 int16_t acc_xbias,acc_ybias,acc_zbias,gyro_xbias,gyro_ybias,gyro_zbias,flag_mpu=0;//零飘校准
 
@@ -31,7 +31,7 @@ void mpu6050estimation_Pitch(float*Pitch)
 //	mpu6050_gyro_y-=0;
 	ax=mpu6050_acc_x-240;
 	az=mpu6050_acc_z-20;
-	gy=mpu6050_gyro_y;
+	gy=mpu6050_gyro_y+85;
 	
 	/*if(mpu6050_acc_x>-5&&mpu6050_acc_x<5)//限幅滤波
 	{
@@ -46,6 +46,18 @@ void mpu6050estimation_Pitch(float*Pitch)
 		mpu6050_gyro_y=0;
 	}*/
 	
+	if(gy>-20&&gy<20)//角速度y限幅
+	{
+		gy=0;
+	}
+//	else if(gy<-20)
+//	{
+//		gy+=20;
+//	}
+//	else if(gy>20)
+//	{
+//		gy-=20;
+//	}		
 	//坐标轴标定
 	
 	acc_x = mpu6050_acc_transition(ax); //加速度计转化为物理量 单位g
@@ -56,21 +68,21 @@ void mpu6050estimation_Pitch(float*Pitch)
 	filteraz(&acc_z,0.9);
 	filtergy(&gyro_y,0.9);
 	//gyro_y1=gyro_y+0.178449;//去零飘
-	gyro_y1=gyro_y+4.996;	
+//	gyro_y1=gyro_y+4.996;	
+	gyro_y1=gyro_y;
 	AY = -atan2(acc_x,acc_z)* 180.0f / 3.14159265f;//-? 得到加速度计算出的角度 °
 	GY = AngleY + gyro_y1*t;		//得到角速度计算出的角度,t角速度积分，和定时中断同步
-//	if(flag_mpu==0)
+//	if(flag_mpu>=0)
 //	{
 		AngleY = AlphaPitch * AY + (1 - AlphaPitch) * GY;//互补滤波
 //	}
 //	else
 //		{
-//			//flag_mpu--;
 //			AngleY=AY;
 //		}
 //	
 	
-	*Pitch=-AngleY+0.4;//赋值给储存Pitch的变量
+	*Pitch=-AngleY-4;//赋值给储存Pitch的变量
 	
 }
 
