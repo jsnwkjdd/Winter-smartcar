a7b59c809d08567740af1a5b8cdf1b6f4cc9c236
1936705527 <3093125288@qq.com>
2026-02-24 13:18:02 +0800

    测试通过版

--- project/user/src/main.c
+++ project/user/src/main.c
@@ -93,20 +93,20 @@ int main(void)
 {
     clock_init(SYSTEM_CLOCK_120M);                                              // 初始化芯片时钟 工作频率为 120MHz
     debug_init();                                                               // 初始化默认 Debug UART
-//	pit_ms_init(PIT, 1);                                                      // 初始化 PIT（TIM6_PIT） 为周期中断 1ms 周期
+	pit_ms_init(PIT, 1);                                                      // 初始化 PIT（TIM6_PIT） 为周期中断 1ms 周期
 	interrupt_set_priority(PIT_PRIORITY, 0);
-//	bluetooth_ch9141_init();
-//	Motor_Init();
+	bluetooth_ch9141_init();
+	Motor_Init();
 	mpu6050_init();
-//	my_key_init();
-//	timer_key();
-//	menu_init();
+	my_key_init();
+	timer_key();
+	menu_init();
 //	menu_load();
-//	Pitch=0;
-//	Encoder_Init();
-//	PID_Init(&AnglePID);
-//	PID_Init(&SpeedPID);
-//	PID_Init(&TurnPID);
+	Pitch=0;
+	Encoder_Init();
+	PID_Init(&AnglePID);
+	PID_Init(&SpeedPID);
+	PID_Init(&TurnPID);
 //	bluetooth_ch9141_init();
 //	Motor_SetSpeedleft(0);
 //	Motor_SetSpeedright(0);
@@ -118,17 +118,18 @@ int main(void)
     while(1)
     {
 		//printf("%f\n",Pitch);
-//		menu_save();
-//		menu_key();
+		menu_save();
+		menu_key();
 //		menu_save();     
 //		printf("[plot,%f]",-Pitch-2);
 //		tft180_show_int(50, 90,AnglePID.Target , 3);
-//		tft180_show_float(0, 90,-Pitch , 2,2);
+		tft180_show_float(0, 90,-Pitch , 2,2);
 //		tft180_show_float(0, 140,encoderleft , 3,2);
 //		tft180_show_float(0, 150,encoderright, 3,2);
 		soft_iic_init(&mpu6050_iic_struct, MPU6050_DEV_ADDR, MPU6050_SOFT_IIC_DELAY, MPU6050_SCL_PIN, MPU6050_SDA_PIN);
 		mpu6050_get_acc(); //读取加速度计初始数据
 		mpu6050_get_gyro(); //读取角速度计初始数据 
+		mpu6050estimation_Pitch(&Pitch);
 		tft180_show_float(0, 100,mpu6050_gyro_y , 2,2); 
 		tft180_show_float(0, 110,mpu6050_acc_x , 2,2); 
 		tft180_show_float(0, 120,mpu6050_acc_z , 2,2);
